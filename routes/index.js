8702e319ab4a5de573be613737b141081fd8331f
Stepan Tytseiko <stepantytseyko@gmail.com>
2023-07-03 10:18:37 +0300

    fixed root route

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,9 @@ var router = express.Router();
 const { parse } = require('rss-to-json');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/', async function(req, res, next) {
-  var rss = await parse('https://lifehacker.com/rss');
-  res.send(rss);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
